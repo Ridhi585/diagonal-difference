aeceb46d70b564f1f550263403738068848d18cd
Ridhi585 <ridhitrehan585@gmail.com>
2025-11-22 23:09:50 +0530

    Add files via upload

---
+++ diagonal.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class diagonal{
+    public static void main(String args[]){
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int[][] arr = new int[n][n];
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        arr[i][j] = sc.nextInt();
+                    }
+                }
+                int primarySum = 0;
+                int secondarySum = 0;
+                for (int i = 0; i < n; i++) {
+                    primarySum += arr[i][i];
+                    secondarySum += arr[i][n - i - 1];
+                }
+                int result = Math.abs(primarySum - secondarySum);
+                System.out.println(result);
+            }
+        }
